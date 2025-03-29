0b9e7ca65497e50bd85ba782ac36204eea504dda
Lagman <tetenggalit80823@gmail.com>
2025-03-29 15:08:43 -0700

    update index.html / css / user_login

--- Website/login/login.js
+++ Website/login/login.js
@@ -9,3 +9,18 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
 })
+
+// hide password
+var pass;
+function pass(){
+    if(pass == 1){
+        document.getElementById('password').type='password';
+        document.getElementById('hidepassword').src='fa-solid fa-eye-slash';
+        pass = 0;
+    }
+    else{
+        document.getElementById('password').type='password';
+        document.getElementById('hidepassword').src='';
+        pass = 1;
+    }
+}
