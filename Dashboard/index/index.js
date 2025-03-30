69385ffa1fd7a4e67e374272bda820b94f95b804
Feeder1129 <mrmm112903@gmail.com>
2025-03-30 23:50:06 +0800

    dashboard active panel

---
+++ Dashboard/index/index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll(".side-bar ul a");
+    const iframe = document.querySelector("iframe");
+
+    if (!document.querySelector(".side-bar ul a.active")) {
+        links[0].classList.add("active");
+        iframe.src = "../Dashboard/Dashboard.html"; 
+    }
+
+    links.forEach(link => {
+        link.addEventListener("click", function (event) {
+
+            links.forEach(l => l.classList.remove("active"));
+
+            this.classList.add("active");
+
+            iframe.src = this.getAttribute("href");
+
+            event.preventDefault();
+        });
+    });
+});
