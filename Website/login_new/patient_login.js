64173460bed3622b0385879f2026a301487ad522
Jarencio <145330400+Jarencio@users.noreply.github.com>
2025-04-16 11:58:20 +0800

    Address Dropdown Options

--- Website/login_new/patient_login.js
+++ Website/login_new/patient_login.js
@@ -66,29 +66,86 @@ function Login(US,PA){
     });
 }
 
-// //COOKIES FOR SECURITY
-// function SetCookie(name, hours, value){
-//     const date = new Date();
-//     date.setTime(date.getTime() + hours * 60 * 60 * 1000); // Convert hours to milliseconds
-//     let expires = "expires=" + date.toUTCString();
-//     document.cookie = `${name}=${value}; ${expires}; path=/`;
-// }
 
-// function DeleteCookie(name){
-//     // Set the cookie with an expiration date in the past to delete it
-//     SetCookie(name, -1, ""); // -1 will expire the cookie
-// }
+function Address() {
+    let MunDataGlobal = []; // To store municipality data for later use
+    let BarangayDataGlobal = []; // To store barangay data for later use
 
-// function GetCookie(name){
-//     const cDecoded = decodeURIComponent(document.cookie);
-//     const Carray = cDecoded.split("; ");
-//     let result = null;
-//     console.log(Carray);
+    Promise.all([
+        fetch('../../Extensions/JSON_ADDRESS/table_region.json').then(res => res.json()),
+        fetch('../../Extensions/JSON_ADDRESS/table_province.json').then(res => res.json()),
+        fetch('../../Extensions/JSON_ADDRESS/table_municipality.json').then(res => res.json()),
+        fetch('../../Extensions/JSON_ADDRESS/table_barangay.json').then(res => res.json())
+    ])
+    .then(([regionData, ProvinceData, MunData, BarangayData]) => {
+        const ProvinceSelect = document.getElementById("province"); 
+        const CitySelect = document.getElementById("city"); 
+        const BarangaySelect = document.getElementById("barangay"); 
 
-//     Carray.forEach(Element=>{
-//         if(Element.indexOf(name)==0){
-//             result = Element.substring(name.length + 1); 
-//         }
-//     })
-//     return result; 
-// }
+        // Save municipality and barangay data globally for later use
+        MunDataGlobal = MunData;
+        BarangayDataGlobal = BarangayData;
+
+        ProvinceSelect.innerHTML = '<option value="">Select a Province</option>';
+        CitySelect.innerHTML = '<option value="">Select a City</option>';
+        BarangaySelect.innerHTML = '<option value="">Select a Barangay</option>';
+
+        CitySelect.disabled = true;
+        BarangaySelect.disabled = true;
+
+        // Populate provinces
+        ProvinceData.forEach(province => {
+            if (province.province_name && province.region_id == 5) {
+                const option = document.createElement("option");
+                option.value = province.province_id; // Use province_id for filtering
+                option.textContent = province.province_name;
+                ProvinceSelect.appendChild(option);
+            }
+        });
+
+        // Event: When Province changes, populate cities
+        ProvinceSelect.addEventListener("change", function () {
+            const selectedProvinceId = this.value;
+            CitySelect.innerHTML = '<option value="">Select a City</option>';
+            BarangaySelect.innerHTML = '<option value="">Select a Barangay</option>';
+            CitySelect.disabled = true;
+            BarangaySelect.disabled = true;
+
+            if (selectedProvinceId) {
+                const filteredCities = MunDataGlobal.filter(city => city.province_id == selectedProvinceId);
+                filteredCities.forEach(city => {
+                    const option = document.createElement("option");
+                    option.value = city.municipality_id; // Use ID for next filtering step
+                    option.textContent = city.municipality_name;
+                    CitySelect.appendChild(option);
+                });
+
+                CitySelect.disabled = false;
+            }
+        });
+
+        // Event: When City changes, populate barangays
+        CitySelect.addEventListener("change", function(){
+            const selectedCityID = this.value;
+            BarangaySelect.innerHTML = '<option value="">Select a Barangay</option>';
+            BarangaySelect.disabled = true;
+
+            if (selectedCityID) {
+                const filteredBarangays = BarangayDataGlobal.filter(barangay => barangay.municipality_id == selectedCityID);
+                filteredBarangays.forEach(barangay => {
+                    const option = document.createElement("option");
+                    option.value = barangay.barangay_id; // Use barangay_id as the value
+                    option.textContent = barangay.barangay_name;
+                    BarangaySelect.appendChild(option);
+                });
+
+                BarangaySelect.disabled = false;
+            }
+        });
+    })
+    .catch(error => console.error('Error fetching address data:', error));
+}
+
+
+
+Address();
